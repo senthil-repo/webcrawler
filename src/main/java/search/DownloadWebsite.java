c3e3aae9a02a70b89e0ccb89ff185d8013161195
MIKI-UK\s.nathan <s.nathan@group-miki.com>
2020-07-09 18:15:05 +0100

    Added more code to mock the respected classes - DownloadWebsite junit

--- src/main/java/search/DownloadWebsite.java
+++ src/main/java/search/DownloadWebsite.java
@@ -6,8 +6,13 @@ import domain.Website;
  * Created by s.nathan on 09/07/2020.
  */
 public class DownloadWebsite {
+    private Website website;
 
     public Website call() {
-        return new Website("");
+        return website;
+    }
+
+    public void setWebsite(Website website) {
+        this.website = website;
     }
 }
--- src/test/java/search/DownloadWebsiteTest.java
+++ src/test/java/search/DownloadWebsiteTest.java
@@ -1,26 +1,49 @@
 package search;
 
 import domain.Website;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by s.nathan on 09/07/2020.
  */
 public class DownloadWebsiteTest {
     DownloadWebsite downloadWebsite= null;
+    @Mock
+    Jsoup jsoup;
+    @Mock
+    Connection connection;
+    @Mock
+    Document document;
 
     @Before
     public void setup() throws Exception{
         downloadWebsite = new DownloadWebsite();
+        PowerMockito.mockStatic(Jsoup.class);
+        when(Jsoup.connect(anyString())).thenReturn(connection);
     }
 
     @Test
     public void testCall() throws Exception{
+        downloadWebsite.setWebsite(getWebsite());
         Website website = downloadWebsite.call();
-        assertTrue(website.getJavaScriptLibraries().size() > 0);
+        assertNotNull(website);
+        assertTrue(" Invalid result ", website.getUrl().equalsIgnoreCase("https://www.nhs.uk"));
+        assertTrue(" Wrong result ", website.getJavaScriptLibraries().size() > 0);
+    }
+
+    private Website getWebsite() {
+        Website website = new Website("https://www.nhs.uk");
+        return website;
     }
 }
