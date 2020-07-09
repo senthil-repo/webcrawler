83cbaca4273fa4beead71a0e07d9d47a83c0ad44
MIKI-UK\s.nathan <s.nathan@group-miki.com>
2020-07-09 17:14:37 +0100

    Initial commit with a Junit class for SearchEngine

---
+++ src/main/java/search/SearchEngine.java
@@ -0,0 +1,14 @@
+package search;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by s.nathan on 09/07/2020.
+ */
+public class SearchEngine {
+    public Set<String> getResult() {
+        return new HashSet<>();
+    }
+}
+
---
+++ src/test/java/search/SearchEngineTest.java
@@ -0,0 +1,26 @@
+package search;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by s.nathan on 09/07/2020.
+ */
+public class SearchEngineTest {
+    SearchEngine searchEngine= null;
+
+    @Before
+    public void setup() {
+        searchEngine = new SearchEngine();
+    }
+
+    @Test
+    public void testGetResult() {
+        Set<String> websites = searchEngine.getResult();
+        assertTrue(" Invalid result ", websites.size() > 0);
+    }
+}
